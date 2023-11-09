6f2abdbe35bab797fcc7ec041c46042784d607a9
adrimartinez750 <amarseg372@iesalmudeyne.es>
2023-11-09 14:14:14 +0100

    Testeando el versionado

---
+++ holaMundo.java
@@ -0,0 +1,8 @@
+/**
+ * Muestra por pantalla la frase "¡Hola mundo!"
+ */
+public class holaMundo{
+    public static void main (String[] args) {
+        system.out.println("¡Hola mundo!";)
+    }
+}
